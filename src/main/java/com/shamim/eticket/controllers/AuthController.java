278fb22727bc1b93d64428ee3e91c4736657811f
Shamim Sarker <mdshamimmansoori7999@gmail.com>
2026-04-16 18:19:11 +0600

    Introduce common attributes

--- src/main/java/com/shamim/eticket/controllers/AuthController.java
+++ src/main/java/com/shamim/eticket/controllers/AuthController.java
@@ -3,7 +3,10 @@ package com.shamim.eticket.controllers;
 import com.shamim.eticket.global.constants.AppUrl;
 import com.shamim.eticket.models.UserRegistration;
 import com.shamim.eticket.services.interfaces.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,20 +17,28 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private static final Logger logger =
+            LoggerFactory.getLogger(AuthController.class);
+
+    @ModelAttribute
+    public void commonAttributes(HttpServletRequest request, Model model) {
+        logger.info("Inside commonAttributes method");
+        model.addAttribute("loginUrl", AppUrl.LOGIN);
+        model.addAttribute("registerUrl", AppUrl.REGISTER);
+        logger.info("Request is: {}", request.getRequestURI());
+    }
 
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String message, Model model) {
+        logger.info("Inside /login..");
         model.addAttribute("message", message);
-        model.addAttribute("loginUrl", AppUrl.LOGIN);
-        model.addAttribute("registerUrl", AppUrl.REGISTER);
+        logger.info("Ending /login..");
         return "shared/login";
     }
 
     @GetMapping("/register")
     public String registerPage(Model model) {
         model.addAttribute("userRegistration", new UserRegistration());
-        model.addAttribute("loginUrl", AppUrl.LOGIN);
-        model.addAttribute("registerUrl", AppUrl.REGISTER);
         return "shared/register";
     }
 
